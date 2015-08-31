2e8e9048ee23254c2b20686b58a035d6e8aef515
weson <641375443@qq.com>
2015-08-31 11:53:23 +0800

    添加序列化工具类

---
+++ src/com/weson/util/serial/SerializeUtil.java
@@ -0,0 +1,51 @@
+package com.weson.util.serial;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class SerializeUtil {
+
+
+    /**
+     * 序列化
+     *
+     * @param object
+     * @return
+     * @throws Exception
+     */
+    public static byte[] serialize(Object object) throws Exception {
+        ObjectOutputStream oos = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            baos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(object);
+            byte[] bytes = baos.toByteArray();
+            return bytes;
+        } catch (Exception e) {
+            throw new Exception("序列化失败！");
+        }
+    }
+
+    /**
+     * 反序列化
+     *
+     * @param bytes
+     * @return
+     * @throws Exception
+     */
+    public static Object unserialize(byte[] bytes) throws Exception {
+        ObjectInputStream ois = null;
+        ByteArrayInputStream bais = null;
+        try {
+            bais = new ByteArrayInputStream(bytes);
+            ois = new ObjectInputStream(bais);
+            return ois.readObject();
+        } catch (Exception e) {
+            throw new Exception("反序列化失败");
+        }
+    }
+
+}
